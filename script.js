c2547daa61a06be1842b6f88317c83c808b57b0b
tomasmaillo <tomasroma64@gmail.com>
2020-06-25 14:51:08 +0200

    Basic stuff from replit

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(document.getElementById("at"), {
+    // Waits 1000ms after typing "First"
+    strings: ['@tomas^1000maillo', 'Hey there! I am using WhatsApp', 'Yooooo', 'Shantay, you stay'],
+    typeSpeed: 50,
+    backSpeed: 40,
+    startDelay: 200,
+    showCursor:false,
+    loop: true
+});
