3ff6afe106985b0770a437e5ad7ae46d54b0a448
tomasmaillo <tomasroma64@gmail.com>
2020-07-01 13:42:11 +0200

    Deleted some files

--- script.js
+++
@@ -1,9 +0,0 @@
-var typed = new Typed(document.getElementById("at"), {
-    // Waits 1000ms after typing "First"
-    strings: ['@tomas^1000maillo', 'Hey there! I am using WhatsApp', 'Yooooo', 'Shantay, you stay'],
-    typeSpeed: 50,
-    backSpeed: 40,
-    startDelay: 200,
-    showCursor:false,
-    loop: true
-});
