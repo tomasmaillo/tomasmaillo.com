46a3ec7070254bba38b260f3c27014a4bb776ec8
tomas <38633386+Tomasroma64@users.noreply.github.com>
2020-10-18 23:21:26 +0200

    💄 General content update

--- scripts.js
+++ scripts.js
@@ -5,32 +5,33 @@ $(document).ready(function() {
 
 // Coloful theme 
 
-$("#change-theme-btn").click(function () {
+$("#change-theme-btn").click(function() {
     updateTheme();
 });
 
 
-function updateTheme(){
+function updateTheme() {
     let color = getRandomColor();
-    document.documentElement.style.setProperty("--bbackground", color + "22");
+    document.documentElement.style.setProperty("--background", color + "22");
     document.documentElement.style.setProperty("--foreground-default", color + "ff");
+    document.documentElement.style.setProperty("--a", color + "44");
 }
 
 
 function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 
 
 // Toggle project filters
 
-$(".key-holder").click(function () {
+$(".key-holder").click(function() {
     $(this).toggleClass($(this).attr('id'));
     $(".project-holder." + $(this).attr('id')).toggle("hidden");
 });
