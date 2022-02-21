fba8a0e6e076a55fdc2b4cd1fea8de1fd9c651f8
tomas <tomasroma64@gmail.com>
2022-02-21 18:55:08 +0000

    trying something new (video background)

--- scripts.js
+++ scripts.js
@@ -1,18 +1,29 @@
 function updateTheme() {
-    let color = getRandomColor();
-    document.documentElement.style.setProperty("--background", color + "22");
-    document.documentElement.style.setProperty("--foreground-default", color + "ff");
-    document.documentElement.style.setProperty("--a", color + "22");
+  let color = getRandomColor();
+  document.documentElement.style.setProperty("--background", color + "44");
+  document.documentElement.style.setProperty(
+    "--foreground-default",
+    color + "ff"
+  );
+  document.documentElement.style.setProperty("--a", color + "22");
 }
 
 updateTheme();
-setInterval(updateTheme, 2000)
+setInterval(updateTheme, 2000);
 
+console.log("here");
+console.log("HEREEEEE");
+console.log("asdasfdfsdfdsf");
+console.log("here");
+console.log("here");
+console.log("HEree");
+console.log("here");
+console.log("here");
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
