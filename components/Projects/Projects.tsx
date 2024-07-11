7b930e6ca6fe6a5302cebfefa55761b47fbf761f
tomas <tomasroma64@gmail.com>
2024-07-11 19:41:51 +0100

    add fan content

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -6,13 +6,46 @@ import {
   CarouselPrevious,
   CarouselNext,
 } from '../ui/carousel'
+import { cn } from '@/lib/utils'
 
-const CarouselImage = ({ src, alt }: { src: string; alt: string }) => {
+const CarouselImage = ({
+  src,
+  alt,
+  addPadding,
+}: {
+  src: string
+  alt: string
+  addPadding?: boolean
+}) => {
   return (
     <img
-      className="object-contain w-full h-full bg-slate-50 rounded-lg"
+      className={cn(
+        'object-contain w-full h-full bg-slate-50 rounded-lg',
+        addPadding && 'p-2'
+      )}
       src={src}
       alt={alt}
+    />
+  )
+}
+
+const CarouselVideo = ({
+  src,
+  addPadding,
+}: {
+  src: string
+  addPadding?: boolean
+}) => {
+  return (
+    <video
+      className={cn(
+        'object-cover w-full h-full bg-slate-50 rounded-lg overflow-hidden',
+        addPadding && 'p-2'
+      )}
+      src={src}
+      autoPlay
+      loop
+      muted
     />
   )
 }
@@ -58,6 +91,24 @@ const Projects = () => {
           </CarouselItem>
           <CarouselItem>
             <CarouselImage src="/ps-browser-2.png" alt="Project 2" />
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+
+      <ProjectTitle>Fan</ProjectTitle>
+      <ProjectDescription>
+        Elit duis consectetur culpa officia qui nostrud commodo in cillum cillum
+        Lorem in ex culpa adipisicing.
+      </ProjectDescription>
+      <Carousel>
+        <CarouselContent>
+          <CarouselItem>
+            <CarouselImage addPadding src="/zf-phone.png" alt="Project 1" />
+          </CarouselItem>
+          <CarouselItem>
+            <CarouselVideo src="/zf-video.mp4" />
           </CarouselItem>
         </CarouselContent>
         <CarouselPrevious />
